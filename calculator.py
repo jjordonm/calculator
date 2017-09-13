a8d90bc194612f0d70349ed6217b31ffdb35493c
Jordon Malcolm <jordonmalcolm15@gmail.com>
2017-09-13 16:52:14 -0400

    Updates

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 #This program creates a simple calculator
 
+
+# Operation functions are created below
+
 def add(x, y):
     return x + y
 
@@ -10,7 +13,23 @@ def divide(x, y):
     return x / y
 
 def multiply(x, y):
-    returm x * y
+    return x * y
 
-def square(x, y)
+def square(x, y):
     return x ** y
+
+def main():
+
+    # Tells the user their operation choices
+
+    print("1) Addition")
+    print("2) Subtraction")
+    print("3) Divide")
+    print("4) Multiply")
+    print("5) Square")
+
+    # Asks the user for their inputs
+
+    operation = int(input("Choose your operation choice: (1/2/3/4/5) "))
+    num1 = int(input("Please enter your first number: "))
+    num2 = int(input("Please enter your second numbebr: "))
