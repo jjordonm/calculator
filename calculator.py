9e2cb9902562b9ade60dc8905f7aa9081d9ea2f8
Jordon Malcolm <jordonmalcolm15@gmail.com>
2017-09-13 16:39:28 -0400

    Basic Functions

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#This program creates a simple calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+def multiply(x, y):
+    returm x * y
+
+def square(x, y)
+    return x ** y
